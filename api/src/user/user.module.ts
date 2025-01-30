42088d87681689b4c793c9580ae5951a27287aa2
mblanchet <mblanchet@forlabs.fr>
2025-01-30 20:39:35 +0100

    add user test crud

--- api/src/user/user.module.ts
+++ api/src/user/user.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UserService } from './service/user.service';
+import { UserController } from './controller/user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './models/user.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  providers: [UserService],
+  controllers: [UserController],
+})
 export class UserModule {}
