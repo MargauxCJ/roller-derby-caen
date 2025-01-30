35640aac01f7dfc3e014e8ac8c42700f4940c7ae
mblanchet <mblanchet@forlabs.fr>
2025-01-30 23:45:59 +0100

    login, get jwt token

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,6 +5,7 @@ import * as process from 'node:process';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { UserModule } from './user/user.module';
       synchronize: true,
     }),
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- api/src/user/controller/user.controller.ts
+++ api/src/user/controller/user.controller.ts
@@ -1,15 +1,35 @@
-import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
-import {UserService} from '../service/user.service';
-import {User} from '../models/user.interface';
-import {Observable} from 'rxjs';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UserService } from '../services/user.service';
+import { User } from '../models/user.interface';
+import { catchError, map, Observable, of } from 'rxjs';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Post()
-  create(@Body() user: User): Observable<User> {
-    return this.userService.create(user);
+  create(@Body() user: User): Observable<User | NonNullable<unknown>> {
+    return this.userService.create(user).pipe(
+      map((user: User) => user),
+      catchError((err) => of({ error: err.message() })),
+    );
+  }
+
+  @Post('login')
+  login(@Body() user: User): Observable<NonNullable<unknown>> {
+    return this.userService.login(user).pipe(
+      map((jwt: string) => {
+        return { access_token: jwt };
+      }),
+    );
   }
 
   @Get(':id')
